cfbf88f23f4b1670b6b8d4f3e347fde03ea06d7b
philleer <philleer@163.com>
2019-04-15 19:45:03 +0800

    another similar problem

--- leetcode/674_findLengthOfLCIS.cc
+++ leetcode/674_findLengthOfLCIS.cc
@@ -30,10 +30,12 @@ public:
 	 *				Explanation: The lcis is [2], its length is 1. 
 	 * Note:		Length of the array will not exceed 10,000.
 	 *=============================================================
-	 */	
+	 */
     int findLengthOfLCIS(vector<int>& nums) {
+        // take special case into account
         if (nums.empty())
             return 0;
+
         int len = 1;
         int maxLen = 1;
         
@@ -49,12 +51,54 @@ public:
                 len = 1;
             }
         }
+        // when increasing continues till the end and jump out
+        // for-loop we need to set the maxLen to len of the last
+        // substring if possible
         if (i == nums.size() && len > maxLen) {
             maxLen = len;
         }
 
         return maxLen;
     }
+
+	/*=============================================================
+	 * Description: Given an arbitrary string, displays the maximum
+	 * 				consecutive increasingly ordered substring.
+	 * Example 1: 	Input: Welcome	Output: Wel
+	 *				Explanation: The longest continuous increasing
+	 * 				subsequence is Wel. Even though Welo is also an
+	 *				increasing subsequence, it's not continuous.
+	 *=============================================================
+	 */
+    string findLongestCIS(string &str) {
+		if (str.empty())
+			return "";
+		int startIndex = 0;
+		int len = 1;
+
+		pair<int, int> p(0, 1);
+		int i = 1;
+		for (; i < str.length(); i++) {
+			if (str[i] != '\0' && str[i] > str[i-1]) {
+				len++;
+			}
+			else {
+				if (len > p.second)
+				{
+					p.first = startIndex;
+					p.second = len;
+				}
+				startIndex = len;
+				len = 1;
+			}
+		}
+		if (i == str.length() && len > p.second) {
+			p.first = startIndex;
+			p.second = len;
+		}
+
+		return str.substr(p.first, p.second);
+	}
 };
 
 int main(int argc, char **argv) {
@@ -69,8 +113,9 @@ int main(int argc, char **argv) {
 		cout << *it << " ";
 	cout << endl;
 	
+	Solution solver;
 	// print the length of longest continuous increasing
 	// subsequence
-	cout << findLengthOfLCIS(vectorTest) << endl;
+	cout << solver.findLengthOfLCIS(vectorTest) << endl;
 	return 0;
 }
