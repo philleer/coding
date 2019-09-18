c323bd5ea49f6ca145e81aa30ca679a9f4a05958
philleer <phillee2016@163.com>
2019-09-18 18:32:18 +0800

    optimize the exponent process

--- problem_and_solution/003_gaussian_kernel.cc
+++ problem_and_solution/003_gaussian_kernel.cc
@@ -1,9 +1,9 @@
 #include <cstdio>
 #include <vector>
 #include <cmath>
-#include <float.h>	// FLT_MAX 3.402823466e+38F
+#include <float.h>		// FLT_MAX 3.402823466e+38F
 #include <stdexcept>	// std::invalid_argument
-#include <limits>	// std::numeric_limits
+#include <limits>		// std::numeric_limits
 
 #define PI 3.141593
 
@@ -17,15 +17,18 @@ public:
 
 void Solution::gaussian_kernel(int size, float sigma, std::vector<std::vector<float>> &res) {
 	if (size <= 0 || sigma < 1e-5) {
-		throw std::invalid_argument(""Invalid size or sigma!);
+		throw std::invalid_argument("Invalid size or sigma!");
 	}
 
 	int bound = size / 2;
 	float sum = 0.0f;
+	// f(x, y) = 1 / (2*pi*sigma) * exp(-(x*x+y*y)/(2*sigma*sigma))
+	// when normalize it is no need for the factor 1/(2*pi*sigma)
 	for (int y = 0; y < size; y++) {
 		std::vector<float> vec_tmp;
 		for (int x = 0; x < size; x++) {
-			float value_tmp = exp(-(fpow(x - bound) + fpow(y - bound))) / (2 * PI * sigma * sigma);
+			float value_tmp = exp(-(fpow(x - bound) + fpow(y - bound)))
+				   /* / (2 * PI * sigma * sigma) */;
 			vec_tmp.push_back(value_tmp);
 			sum += value_tmp;
 		}
@@ -68,3 +71,4 @@ int main(int argc, char *argv[]) {
 
 	return 0;
 }
+
