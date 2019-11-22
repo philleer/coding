cd5bc30454692e85de7fed2cfe6543272f0cafbc
philleer <philleer@163.com>
2019-11-22 14:53:58 +0800

    add backtrack solution solve N-Queen problem

---
+++ leetcode/012_013.intToRoman.cc
@@ -0,0 +1,127 @@
+/******************************************************************************
+ * The source is part of my coding practice, a lightweight C++ solution for
+ * leetcode program practice.
+ *
+ * For all of my code, after check it online by submit, I also compile and run
+ * it by my offline machine
+ * ============================================================================
+ * ////////////// Microsoft Visual Studio 2010 when I use windows /////////////
+ * //////////////		   G++5.4.0 when I am on Linux           /////////////
+ * ============================================================================
+ *
+ * If any question, feel free to send me an email << phillee2016@163.com >>
+ * or just correct it via Pull Request or create an issue.
+ ******************************************************************************
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+class Solution {
+public:
+	// 13. Roman to Integer
+	/*=========================================================================
+	 * Description: Roman numerals are represented by seven different symbols:
+	 *	I, V, X, L, C, D and M, which means value 1, 5, 10, 50, 100, 500, 1000
+	 * 	respectively.
+	 * Note:
+	 *	e.g., 2 is written as II in Roman numeral, two one's added together.
+	 *	12 is written as, XII, which is simply X + II. Number 27 is written as
+	 *	XXVII, which is XX + V + II.
+	 *	Roman numerals are usually written largest to smallest from left to
+	 *	right. However, the numeral for 4 is not IIII. Instead, it is written
+	 *	as IV. Because the one is before the five we subtract it making four.
+	 *	The same principle applies to the number 9, which is written as IX.
+	 *	There are six instances where subtraction is used:
+	 *		I can be placed before V (5) and X (10) to make 4 and 9.
+	 *		X can be placed before L (50) and C (100) to make 40 and 90.
+	 *		C can be placed before D (500) and M (1000) to make 400 and 900.
+	 *	Given a roman numeral, convert it to an integer. Input is guaranteed
+	 *	to be within the range from 1 to 3999.
+	 * Example:
+	 *	Input: "III"	Output: 3
+	 *	Input: "IV"	Output: 4
+	 *	Input: "IX"	Output: 9
+	 *	Input: "LVIII"	Output: 58 	Explanation: L = 50, V= 5, III = 3.
+	 *	Input: "MCMXCIV"Output: 1994
+	 *	Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+	 *=========================================================================
+	 */
+	int romanToInt(std::string s) {
+		std::unordered_map<char, int> lut{{'I', 1}, {'V', 5}, {'X', 10},
+						  {'L', 50}, {'C', 100}, {'D', 500},
+						  {'M', 1000}};
+		
+		int result = 0;
+		int len = s.length();
+		for (int i = 0; i < len; ++i) {
+			if (i+1 < len && lut[s[i]] < lut[s[i+1]]) {
+				result -= lut[s[i]];
+				continue;
+			}
+			result += lut[s[i]];
+		}
+		
+		return result;
+	}
+
+	// 12. Integer to Roman
+	/*=========================================================================
+	 * Description: The same as above
+	 * Example:
+	 *	Input: 3	Output: "III"
+	 *	Input: 4	Output: "IV"
+	 *	Input: 9	Output: "IX"
+	 *	Input: 58	Output: "LVIII"
+	 *	Explanation: L = 50, V = 5, III = 3.
+	 *
+	 *	Input: 1994	Output: "MCMXCIV"
+	 *	Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+	 *=========================================================================
+	 */
+	std::string intToRoman(int num) {
+		std::string ret;
+		if (num < 1 || num > 3999) return ret;
+		
+		std::vector<std::pair<char, int>> lut{{'I', 1}, {'V', 5}, {'X', 10},
+						      {'L', 50}, {'C', 100}, {'D', 500},
+						      {'M', 1000}};
+		
+		for (int i = lut.size()-1; i >= 0; i -= 2) {
+			int tmp = num / lut[i].second;
+			num %= lut[i].second;
+			
+			if (9==tmp || 4==tmp) {
+				ret += lut[i].first;
+				ret += (9==tmp ? lut[i+2].first : lut[i+1].first);
+			} else if (tmp > 0) {
+				if (tmp >= 5) {
+					ret += lut[i+1].first;
+					tmp -= 5;
+				}
+				while(tmp--) {
+					ret += lut[i].first;
+				}
+			}
+		}
+		
+		return ret;
+	}
+};
+
+int main(int argc, char *argv[]) {
+	Solution solver;
+
+	std::string str = "MCMXCIV";
+	int ret = solver.romanToInt(str);
+	std::string rev = solver.intToRoman(ret);
+
+	std::cout << "original Roman string: " << str << std::endl;
+	std::cout << "transfered int: " << ret << std::endl;
+	std::cout << "the reversed roman string: " << rev << std::endl;
+
+	return 0;
+}
+
---
+++ leetcode/707_designLinkedList.cc
@@ -0,0 +1,205 @@
+/******************************************************************************
+ * The source is part of my coding practice, a lightweight C++ solution for
+ * leetcode program practice.
+ *
+ * For all of my code, after check it online by submit, I also compile and run
+ * it by my offline machine
+ * ============================================================================
+ * ////////////// Microsoft Visual Studio 2010 when I use windows /////////////
+ * //////////////		   G++5.4.0 when I am on Linux	          /////////////
+ * ============================================================================
+ *
+ * If any question, feel free to send me an email << phillee2016@163.com >>
+ * or just correct it via Pull Request or create an issue.
+ ******************************************************************************
+ */
+#include <iostream>
+#include <vector>
+
+// 707. Design Linked List
+/*=========================================================================
+ * Description: Design your implementation of the linked list. You can
+ *	choose to use the singly linked list or the doubly linked list. A node
+ *	in a singly linked list should have two attributes: val and next.
+ *	val is the value of the current node, and next is a pointer to the next
+ *	node. If you want to use the doubly linked list, you will need one more
+ *	attribute prev to indicate the previous node in the linked list. Assume
+ *	all nodes in the linked list are 0-indexed.
+ *
+ *	Implement these functions in your linked list class:
+ *		get(index) : Get the value of the index-th node in the linked list.
+ *				If the index is invalid, return -1.
+ *		addAtHead(val) : Add a node of value val before the first element of
+ *				the linked list. After the insertion, the new node will be
+ *				the first node of the linked list.
+ *		addAtTail(val) : Append a node of value val to the last element of
+ *				the linked list.
+ *		addAtIndex(index, val) : Add a node of value val before the index-th
+ *				node in the linked list. If index equals to the length of
+ *				linked list, the node will be appended to the end of linked
+ *				list. If index is greater than the length, the node will not
+ *				be inserted.
+ *		deleteAtIndex(index) : Delete the index-th node in the linked list,
+ *				if the index is valid. 
+ *
+ * Example:
+ *	Input: ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", 
+ *			"deleteAtIndex","get"]
+ *	       [[],[1],[3],[1,2],[1],[1],[1]]
+ *	Output: [null,null,null,null,2,null,3]
+ *	Explanation: MyLinkedList linkedList = new MyLinkedList(); 
+ *		     // Initialize empty LinkedList
+ *		linkedList.addAtHead(1);
+ *		linkedList.addAtTail(3);
+ *		linkedList.addAtIndex(1, 2);  // linked list becomes 1->2->3
+ *		linkedList.get(1);	      // returns 2
+ *		linkedList.deleteAtIndex(1);  // now the linked list is 1->3
+ *		linkedList.get(1);            // returns 3
+ *
+ * Constraints:
+ *	0 <= index, val <= 1000;
+ *	Please do not use the built-in LinkedList library.
+ *	At most 2000 calls will be made to get, addAtHead, addAtTail,
+ *	addAtIndex and deleteAtIndex.
+ *=========================================================================
+ */
+class Node {
+public:
+	int val;
+	Node *next;
+	Node() { next = nullptr; }
+	Node(int _val):val(_val),next(nullptr){}
+};
+
+class MyLinkedList {
+private:
+	Node *head;
+	Node *tail;
+	int size;
+
+public:
+    /*
+     * Initialize your data structure here.
+     */
+    MyLinkedList() {
+	head = nullptr;
+	tail = nullptr;
+	size = 0;
+    }
+    
+    /*
+     * Get the value of the index-th node in the linked list. If the index is
+     * invalid, return -1.
+     */
+    int get(int index) {
+	if (index < 0 || index > size - 1) return -1;
+	
+	Node *current = head;
+	for (int i = 0; i < index; i++) current = current->next;
+	return current->val;
+    }
+    
+    /*
+     * Add a node of value val before the first element of the linked list.
+     * After the insertion, the new node will be the first node of the linked
+     * list.
+     */
+    void addAtHead(int val) {
+	Node *newNode = new Node(val);
+	newNode->next = head;
+	head = newNode;
+	size++;
+	
+	if (tail == nullptr) tail = head;
+    }
+    
+    /*
+     * Append a node of value val to the last element of the linked list.
+     */
+    void addAtTail(int val) {
+	if (tail == nullptr) {
+		head = tail = new Node(val);
+	} else {
+		tail->next = new Node(val);
+		tail = tail->next;
+	}
+	size++;
+    }
+    
+    /*
+     * Add a node of value val before the index-th node in the linked list. If
+     * index equals to the length of linked list, the node will be appended to
+     * the end of linked list. If index is greater than the length, the node
+     * will not be inserted.
+     */
+    void addAtIndex(int index, int val) {
+	if (index < 0) return;
+
+	if (index == 0) {
+		addAtHead(val);
+	} else {
+		if (index <= size) {
+			if(index == size) {
+				tail->next = new Node(val);
+				tail = tail->next;
+			} else {
+				Node *current = head;
+				for (int i = 1; i < index; i++) current = current->next;
+				Node *temp = current->next;
+				current->next = new Node(val);
+				(current->next)->next = temp;
+			}
+			size++;
+		}
+	}
+    }
+    
+    /*
+     * Delete the index-th node in the linked list, if the index is valid.
+     */
+    void deleteAtIndex(int index) {
+	if (index < 0 || index > size -1) return;
+	if (index == 0) {
+		if (size == 0) return;
+
+		head = head->next;
+		if (head == nullptr) tail = nullptr;
+		size--;
+	} else {
+		if (index == size - 1) {
+			if (size == 1) {
+				head = tail = nullptr;
+				size = 0;
+			} else {
+				Node *current = head;
+				for (int i = 1; i < index; i++) current = current->next;
+				Node *temp = tail;
+				tail = current;
+				tail->next = nullptr;
+				size--;
+				delete temp;
+			}
+		} else {
+			Node *previous = head;
+			for (int i = 1; i < index; i++) previous = previous->next;
+			Node *temp = previous->next;
+			previous->next = (previous->next)->next;
+			size--;
+			delete temp;
+		}
+	}
+    }
+};
+
+int main(int argc, char *argv[]) {
+	MyLinkedList* obj = new MyLinkedList();
+ 	int param_1 = obj->get(index);
+ 	for (int i = 0; i < 5; ++i) obj->addAtHead(i);
+ 	for (int i = 0; i < 3; ++i) obj->addAtTail(i);
+
+ 	obj->addAtIndex(5, 12);
+ 	obj->deleteAtIndex(5);
+
+	return 0;
+}
+
