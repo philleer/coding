eb1b87e17a3f8d4f9b910a9a90f93e6c4864e6ea
philleer <phillee2016@163.com>
2019-11-28 21:34:56 +0800

    change some format to make it more formal

--- leetcode/344_reverseString.cc
+++ leetcode/344_reverseString.cc
@@ -6,7 +6,7 @@
  * it by my offline machine
  * ============================================================================
  * ////////////// Microsoft Visual Studio 2010 when I use windows /////////////
- * //////////////		   G++5.4.0 when I am on Linux           /////////////
+ * //////////////           G++5.4.0 when I am on Linux           /////////////
  * ============================================================================
  *
  * If any question, feel free to send me an email << phillee2016@163.com >>
@@ -20,18 +20,20 @@
 class Solution {
 public:
 	// 344. Reverse String
-	/*=========================================================================
-	 * Description: Write a function that reverses a string. The input string
-	 *	is given as an array of characters char[].
-	 *	Do not allocate extra space for another array, you must do this by
-	 *	modifying the input array in-place with O(1) extra memory.
-	 *	You may assume all the characters consist of printable ascii characters.
+	/*=====================================================================
+	 * Description: Write a function that reverses a string.
+	 *	The input string is given as an array of characters char[].
+	 *	Do not allocate extra space for another array, you must do this
+	 *	by modifying the input array in-place with O(1) extra memory.
+	 *	You may assume all the characters consist of printable ascii
+	 *	characters.
 	 *
 	 * Example:
-	 *	Input: ["h","e","l","l","o"] 	Output: ["o","l","l","e","h"]
+	 *	Input: ["h","e","l","l","o"] 	 Output: ["o","l","l","e","h"]
 	 *
-	 *	Input: ["H","a","n","n","a","h"] 	Output: ["h","a","n","n","a","H"]
-	 *=========================================================================
+	 *	Input: ["H","a","n","n","a","h"]
+	 *	Output: ["h","a","n","n","a","H"]
+	 *=====================================================================
 	 */
 	void reverseString(std::vector<char> &s) {
 		if (s.empty()) return;
@@ -46,15 +48,13 @@ int main(int argc, char *argv[]) {
 
 	std::vector<char> str{'h', 'e', 'l', 'l', 'o'};
 	solver.reverseString(str);
+	
 	std::cout << "original string: " << std::endl;
-	for (auto &c : str) {
-		std::cout << c << " ";
-	}
+	for (auto &c : str) std::cout << c << " ";
 	std::cout << std::endl;
+	
 	std::cout << "the reversed string: " << std::endl;
-	for (auto &c : str) {
-		std::cout << c << " ";
-	}
+	for (auto &c : str) std::cout << c << " ";
 	std::cout << std::endl;
 
 	return 0;
