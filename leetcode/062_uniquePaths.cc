f8e13bc214c6b30d1b7f2b9d290082ca222487b7
philleer <phillee2016@163.com>
2019-12-06 10:46:03 +0800

    add unique path II solution

--- leetcode/062_uniquePaths.cc
+++ leetcode/062_uniquePaths.cc
@@ -56,6 +56,47 @@ public:
 
 		return dp[m-1][n-1];
 	}
+	// 63. Unique Paths II
+	/*=====================================================================
+	 * Description: Now consider if some obstacles are added to the grids.
+	 *	How many unique paths would there be?
+	 *	An obstacle and empty space is marked as 1 and 0 respectively
+	 *	in the grid.
+	 *
+	 * Example:
+	 *	Input:[[0,0,0], [0,1,0], [0,0,0]]	Output: 2
+	 *	Explanation: There's one obstacle in the middle of the 3x3 grid
+	 *	above. There are two ways to reach the bottom-right corner:
+	 *		1. Right -> Right -> Down -> Down
+	 *		2. Down -> Down -> Right -> Right
+	 *=====================================================================
+	 */
+	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+		if (obstacleGrid.empty() || obstacleGrid[0][0]==1) return 0;
+
+		int m = obstacleGrid.size();
+		int n = obstacleGrid[0].size();
+		std::vector<std::vector<long long>> dp(m,
+						       std::vector<long long>(
+						       n, 0));
+
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (obstacleGrid[i][j]==0) {
+					if (i==0 || j==0) {
+						dp[i][j] = (i==0 ? (
+							j==0 ? 1 : dp[i][j-1])
+							: dp[i-1][j]);
+					} else {
+						dp[i][j] = dp[i-1][j] +
+							   dp[i][j-1];
+					}
+				}
+			}
+		}
+
+		return dp[m-1][n-1];
+	}
 };
 
 int main(int argc, char const *argv[]) {
